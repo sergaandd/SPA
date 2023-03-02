f644910ad259d6f6f191ba6b14f139cf61c1e655
Serhii Beseda <sergaandd@gmail.com>
2023-03-02 22:04:56 +0200

    add dynamic data

--- src/app/service/product-handler.service.ts
+++ src/app/service/product-handler.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {Product} from "../model/Product";
+import {TestData} from "../data/TestData";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class ProductHandlerService {
 
   constructor() { }
+
+  getProducts(): Product[]{
+    return TestData.products;
+}
 }
